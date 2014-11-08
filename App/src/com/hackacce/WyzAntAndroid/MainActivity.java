5503b714c201769c5a3e47bef73b2a1eb20ee01d
unknown <nate.cooper.90@gmail.com>
2014-11-08 03:48:47 -0800

    more layouting and string definitions

--- App/src/com/hackacce/WyzAntAndroid/MainActivity.java
+++ App/src/com/hackacce/WyzAntAndroid/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.WyzAntAndroid;
+package com.hackacce.WyzAntAndroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +13,23 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Spinner distSpinner = (Spinner)findViewById(R.id.distField);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> distAdapter =
+                ArrayAdapter.createFromResource(this, R.array.options_distance, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        distAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        distSpinner.setAdapter(distAdapter);
+
+        Spinner genderSpinner = (Spinner)findViewById(R.id.genderField);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> genderAdapter =
+                ArrayAdapter.createFromResource(this, R.array.options_gender, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        genderSpinner.setAdapter(genderAdapter);
     }
 }
