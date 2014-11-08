eb89e82974074533eab164d7ab62fdc26f433cea
Lckythr33 <Zeeshawn1@gmail.com>
2014-11-08 13:05:21 -0800

    With user input and browser intent

--- App/src/com/hackacce/WyzAntAndroid/MainActivity.java
+++ App/src/com/hackacce/WyzAntAndroid/MainActivity.java
@@ -1,14 +1,19 @@
 package com.hackacce.WyzAntAndroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
-     */
+        */
+
+    Button mStringBuilder;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +36,28 @@ public class MainActivity extends Activity {
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         genderSpinner.setAdapter(genderAdapter);
+        EditText subjectField = (EditText) findViewById(R.id.subjectField);
+        EditText zipField = (EditText) findViewById(R.id.zipField);
+        Spinner distField = (Spinner) findViewById(R.id.distField);
+        mStringBuilder = (Button)findViewById(R.id.search);
+        mStringBuilder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//Convert user input to string
+               String query ="http://www.wyzant.com/tutorsearch?kw="+ subjectField.getText().toString()
+                       + "&z=" + zipField.getText().toString()
+                       + "&d=" + distField.getSelectedItem().toString();
+              TextView test = (TextView)findViewById(R.id.queryPreview);
+                test.setText(query);
+
+//launches browser intent
+
+                String url = query;
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
