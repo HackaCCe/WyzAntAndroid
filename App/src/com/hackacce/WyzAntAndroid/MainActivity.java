4d986d04e9bbeead7f218ac4cf9c2d9cfe8e32bc
Lckythr33 <Zeeshawn1@gmail.com>
2014-11-08 22:59:53 -0800

    Fixed Broken Links

--- App/src/com/hackacce/WyzAntAndroid/MainActivity.java
+++ App/src/com/hackacce/WyzAntAndroid/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        Spinner distSpinner = (Spinner)findViewById(R.id.distField);
+        Spinner distSpinner = (Spinner) findViewById(R.id.distField);
         // Create an ArrayAdapter using the string array and a default spinner layout
         final ArrayAdapter<CharSequence> distAdapter =
                 ArrayAdapter.createFromResource(this, R.array.options_distance, android.R.layout.simple_spinner_item);
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
         // Apply the adapter to the spinner
         distSpinner.setAdapter(distAdapter);
 
-        final Spinner genderSpinner = (Spinner)findViewById(R.id.genderField);
+        final Spinner genderSpinner = (Spinner) findViewById(R.id.genderField);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> genderAdapter =
                 ArrayAdapter.createFromResource(this, R.array.options_gender, android.R.layout.simple_spinner_item);
@@ -44,34 +44,48 @@ public class MainActivity extends Activity {
 
         final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
 
+        ImageView mBanner;
+        mBanner = (ImageView) findViewById(R.id.imageBanner);
+        mBanner.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent i = new Intent(Intent.ACTION_VIEW);
+                   i.setData(Uri.parse(getString(R.string.baseUrl)));
+                   startActivity(i);
+               }
+           });
+
         Button mStringBuilder;
-        mStringBuilder = (Button)findViewById(R.id.search);
+        mStringBuilder = (Button) findViewById(R.id.search);
         mStringBuilder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-        //Convert user input to string
+                //Convert user input to string
 
-            StringBuilder query = new StringBuilder(R.string.baseQuery)
-                .append("?kw=").append(subjectField.getText().toString())
-                .append("&z=").append(zipField.getText().toString())
-                .append("&d=").append(distSpinner.getSelectedItem().toString())
-                .append("&mina=").append(ageLowerField.getText().toString())
-                .append("&maxa=").append(ageUpperField.getText().toString())
-                .append("&maxh=").append(rateUpperField.getText().toString())
-                .append("&minh=").append(rateLowerField.getText().toString())
-                .append("&im=").append(genderSpinner.getLastVisiblePosition() - 1)
-                .append("&bgCheck=").append(checkBox.isChecked() ? "true" : "false");
+                StringBuilder query = new StringBuilder (getString(R.string.baseUrl)).append(getString(R.string.baseQuery))
+                        .append("?kw=").append(subjectField.getText().toString())
+                        .append("&z=").append(zipField.getText().toString())
+                        .append("&d=").append(distSpinner.getSelectedItem().toString())
+                        .append("&mina=").append(ageLowerField.getText().toString())
+                        .append("&maxa=").append(ageUpperField.getText().toString())
+                        .append("&maxh=").append(rateUpperField.getText().toString())
+                        .append("&minh=").append(rateLowerField.getText().toString())
+                        .append("&im=").append(genderSpinner.getLastVisiblePosition() - 1)
+                        .append("&bgCheck=").append(checkBox.isChecked() ? "true" : "false");
 
-            TextView test = (TextView) findViewById(R.id.queryPreview);
-            test.setText(query);
+                TextView test = (TextView) findViewById(R.id.queryPreview);
+                test.setText(query);
 
-        // Launches browser intent
+                // Launches browser intent
 
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(query.toString()));
-            startActivity(i);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(query.toString()));
+                startActivity(i);
             }
+
         });
     }
 }
+
+
